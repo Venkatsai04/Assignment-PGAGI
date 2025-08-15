a36cafa04cea13e442dc605f9a9513523a058345
venkat sai <saik87630@gmail.com>
2025-08-15 23:22:08 +0530

    chnages

--- src/App.jsx
+++ src/App.jsx
@@ -161,7 +161,7 @@ function App() {
                     items={articles.map(a => a._id)}
                     strategy={verticalListSortingStrategy}
                   >
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
                       <AnimatePresence>
                         {articles.length ? (
                           articles.map((item) => (
